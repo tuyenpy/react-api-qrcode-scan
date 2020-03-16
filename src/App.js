b975c34a3ec739143d36ad67328bd2910845b1a8
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-03-16 15:57:17 +0700

    Add: Axios lib

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  let [state, setState] = useState({
+    logs: []
+  });
+  const checkHistory = () => {
+    axios.get(`https://express-api-demo.herokuapp.com/api/checkinHistory`)
+      .then(res => {
+        setState({ logs: res.data });
+      })
+      .catch(error => console.log(error));
+  };
+  const postQRcode = () => {
+    axios.post('https://express-api-demo.herokuapp.com/api/qrcode/', 
+    {
+      id: 3
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+  let {logs} = state;
+  console.log(state);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick = {checkHistory}>Click here</button>
+        <>
+          {
+            logs&&logs.map((item,index) => {
+              return (
+              <tr key = {index}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.date}</td>
+              </tr>)
+            })
+          }
+        </>
+        <button onClick = {postQRcode}>Post QRcode</button>
     </div>
   );
 }
